ff985b1853c121586082b35299cd1fe1cd07fe62
Crystal Chun <crystalchun@crystals-mbp.raleigh.ibm.com>
2019-07-18 09:48:43 -0400

    Revert reason back to how it was.

--- pkg/controller/grcpolicy/grcpolicy_controller.go
+++ pkg/controller/grcpolicy/grcpolicy_controller.go
@@ -535,7 +535,7 @@ func createParentPolicyEvent(instance *mcmv1alpha1.CertPolicy) {
 
 	parentPlc := createParentPolicy(instance)
 
-	reconcilingAgent.recorder.Event(&parentPlc, corev1.EventTypeNormal, fmt.Sprintf("Policy %s/%s is %v;", instance.Namespace, instance.Name, instance.Status.ComplianceState), convertPolicyStatusToString(instance, DefaultDuration))
+	reconcilingAgent.recorder.Event(&parentPlc, corev1.EventTypeNormal, fmt.Sprintf("Policy %s/%s", instance.Namespace, instance.Name), convertPolicyStatusToString(instance, DefaultDuration))
 }
 
 func createParentPolicy(instance *mcmv1alpha1.CertPolicy) mcmv1alpha1.Policy {
