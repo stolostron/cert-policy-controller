8bfd4875623fc22cb2ed775ad63f499d502ced35
Crystal Chun <crystalchun@Crystals-MBP.attlocal.net>
2019-07-24 11:03:54 -0400

    Added and updated copyright information.

--- pkg/controller/util/certificates_util.go
+++ pkg/controller/util/certificates_util.go
@@ -1,3 +1,8 @@
+// Licensed Materials - Property of IBM
+// (c) Copyright IBM Corporation 2018, 2019. All Rights Reserved.
+// Note to U.S. Government Users Restricted Rights:
+// Use, duplication or disclosure restricted by GSA ADP Schedule
+// Contract with IBM Corp.
 package util
 
 import (
