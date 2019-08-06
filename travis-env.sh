13800acd3c57da5a3bf61248fc24950cbdf19160
Crystal Chun <crystalchun@crystals-mbp.raleigh.ibm.com>
2019-08-06 11:13:29 -0400

    Changed image version for master pushes to be 3.2.1.

--- travis-env.sh
+++ travis-env.sh
@@ -2,7 +2,7 @@
 if [ "$TRAVIS_BRANCH" = "master" ] && ! [ "$TRAVIS_EVENT_TYPE" = "pull_request" ]; then
     RETAG=true
     RELEASE_TAG=latest
-    IMAGE_VERSION=4.1.0
+    IMAGE_VERSION=3.2.1
     DOCKER_REGISTRY=hyc-cloud-private-integration-docker-local.artifactory.swg-devops.com
     NAMESPACE=ibmcom
 
