db6d5b238e3b97bdf6eba7e7d43038cbfe1d7d67
Crystal Chun <crystal.chun@ibm.com>
2019-10-11 11:33:28 -0400

    Updated image version.

--- travis-env.sh
+++ travis-env.sh
@@ -2,7 +2,7 @@
 if [ "$TRAVIS_BRANCH" = "master" ] && ! [ "$TRAVIS_EVENT_TYPE" = "pull_request" ]; then
     RETAG=true
     RELEASE_TAG=latest
-    IMAGE_VERSION=3.2.1
+    IMAGE_VERSION=3.2.2
     DOCKER_REGISTRY=hyc-cloud-private-integration-docker-local.artifactory.swg-devops.com
     NAMESPACE=ibmcom
 
