bbc9c21034a5eaa84bb35f7178f8ad5ceb99e75e
Gus Parvin <gparvin@redhat.com>
2020-12-03 12:23:28 -0500

    add test to cover multiple namespace scenario

--- pkg/controller/certificatepolicy/certificatepolicy_utils.go
+++ pkg/controller/certificatepolicy/certificatepolicy_utils.go
@@ -1,8 +1,3 @@
-// Licensed Materials - Property of IBM
-// (c) Copyright IBM Corporation 2018, 2019. All Rights Reserved.
-// Note to U.S. Government Users Restricted Rights:
-// Use, duplication or disclosure restricted by GSA ADP Schedule
-// Contract with IBM Corp.
 // Copyright (c) 2020 Red Hat, Inc.
 
 package certificatepolicy
