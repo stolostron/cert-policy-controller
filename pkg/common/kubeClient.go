83e0a6f708d7cc42aba270b5d0a5ff6545c603a6
ckandag <ckandaga@redhat.com>
2021-06-22 17:21:41 -0500

    lease api updates
    Signed-off-by: ckandag <ckandaga@redhat.com>

--- pkg/common/kubeClient.go
+++ pkg/common/kubeClient.go
@@ -8,8 +8,14 @@
 package common
 
 import (
+	"context"
+	base64 "encoding/base64"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog"
+	"regexp"
 )
 
 // KubeClient a k8s client used for k8s native resources
@@ -18,9 +24,46 @@ var KubeClient *kubernetes.Interface
 // KubeConfig is the given kubeconfig at startup
 var KubeConfig *rest.Config
 
+var HubConfig *rest.Config
+
 // Initialize to initialize some controller varaibles
 func Initialize(kClient *kubernetes.Interface, cfg *rest.Config) {
 
 	KubeClient = kClient
 	KubeConfig = cfg
+}
+
+func LoadHubConfig(namespace string, secretname string) (*rest.Config, error) {
+
+	if HubConfig == nil {
+
+		secretsClient := (*KubeClient).CoreV1().Secrets(namespace)
+		hubSecret, err := secretsClient.Get(context.TODO(), secretname, metav1.GetOptions{})
+
+		if err != nil {
+			klog.Errorf("Error Getting HubConfig Secret:  %v", err)
+			return nil, err
+		}
+
+		secretkconfig := string(hubSecret.Data["kubeconfig"])
+		crt := base64.StdEncoding.EncodeToString(hubSecret.Data["tls.crt"])
+		key := base64.StdEncoding.EncodeToString(hubSecret.Data["tls.key"])
+
+		re := regexp.MustCompile(`(client-certificate:\s+tls.crt)`)
+		secretkconfig = re.ReplaceAllString(secretkconfig, "client-certificate-data: "+crt)
+
+		re = regexp.MustCompile(`(client-key:\s+tls.key)`)
+		secretkconfig = re.ReplaceAllString(secretkconfig, "client-key-data: "+key)
+
+		klog.Errorf("After Secret Value: %v", string(secretkconfig))
+
+		HubConfig, err = clientcmd.RESTConfigFromKubeConfig([]byte(secretkconfig))
+		if err != nil {
+			klog.Errorf("Error getting Rest config for Hub:  %v", err)
+			return nil, err
+		}
+
+		klog.Errorf("HubConfig: %v", HubConfig)
+	}
+	return HubConfig, nil
 }
