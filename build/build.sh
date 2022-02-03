ae3e28262fb3deb6515942fd860bb9e93d07cb27
Dale Haiducek <dhaiduce@redhat.com>
2022-02-03 09:57:18 -0500

    Remove build harness (#129)
    Signed-off-by: Dale Haiducek <dhaiduce@redhat.com>

--- build/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Copyright Contributors to the Open Cluster Management project
-
-set -e
-
-export DOCKER_IMAGE_AND_TAG=${1}
-export GOARCH=$(go env GOARCH)
-make build
-make docker/build
--- build/run-e2e-tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Copyright Contributors to the Open Cluster Management project
-
-set -e
-
-export DOCKER_IMAGE_AND_TAG=${1}
-# make docker/run
