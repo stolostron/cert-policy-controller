e36fad7b74bc997df458177b1ae72150ff5f88c3
Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
2023-07-31 12:46:44 -0400

    [release-2.6] Update to Go v1.20
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module open-cluster-management.io/cert-policy-controller
 
-go 1.19
+go 1.20
 
 require (
 	github.com/go-logr/zapr v1.2.3
--- pkg/common/namespace_selection_test.go
+++ pkg/common/namespace_selection_test.go
@@ -190,7 +190,7 @@ func TestGetSelectedNamespaces(t *testing.T) {
 		t.Run(
 			name,
 			func(t *testing.T) {
-				actual, err := GetSelectedNamespaces(test.selector)
+				actual, err := GetSelectedNamespaces(context.TODO(), test.selector)
 				if err != nil {
 					if test.errMsg == "" {
 						t.Fatalf("Encountered unexpected error: %v", err)
@@ -342,7 +342,7 @@ func TestGetAllNamespaces(t *testing.T) {
 		t.Run(
 			name,
 			func(t *testing.T) {
-				actual, err := GetAllNamespaces(test.labelSelector)
+				actual, err := GetAllNamespaces(context.TODO(), test.labelSelector)
 				if err != nil {
 					if test.errMsg == "" {
 						t.Fatalf("Encountered unexpected error: %v", err)
